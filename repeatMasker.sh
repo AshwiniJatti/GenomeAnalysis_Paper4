48232dd66fee86e1935484053af5cc0ae3d9d5f7
Ashwini Jatti <ashwi@rackham3.uppmax.uu.se>
2022-05-11 10:40:30 +0200

    files

---
+++ repeatMasker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2022-2-5
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 4
+#SBATCH -t 1:00:00
+#SBATCH -J repeatMasker
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user ashwini.jatti.4265@student.uu.se
+
+#modules
+module load bioinfo-tools
+module load RepeatMasker/4.1.2-p1
+
+#commands
+RepeatMasker --species arabidopsis /home/ashwi/genome_analysis/data/analyses/pilon/pilon_correction.fasta 
